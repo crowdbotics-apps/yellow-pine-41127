120a8cd636d850abfad288a8998b00703956fcea
Crowdbotics <team@crowdbotics.com>
2023-05-31 15:24:07 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "yellow-pine-41127",
+  url: "https://yellow-pine-41127.botics.co",
+  api: "https://yellow-pine-41127.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
